ad6f1bf82f40f8f4dcd887f22b750c94b13a0c32
ilyasBayram <ilyas.bayram@hotmail.com>
2022-07-17 21:35:07 +0300

    Add project files.

---
+++ PartyElection/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace PartyElection
+{
+    partial class FormElection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblAParty = new System.Windows.Forms.Label();
+            this.lblBParty = new System.Windows.Forms.Label();
+            this.lblCParty = new System.Windows.Forms.Label();
+            this.lblDParty = new System.Windows.Forms.Label();
+            this.lblEParty = new System.Windows.Forms.Label();
+            this.txBoxAParty = new System.Windows.Forms.TextBox();
+            this.txBoxParty = new System.Windows.Forms.TextBox();
+            this.txBoxEParty = new System.Windows.Forms.TextBox();
+            this.txBoxDParty = new System.Windows.Forms.TextBox();
+            this.txCParty = new System.Windows.Forms.TextBox();
+            this.btnVote = new System.Windows.Forms.Button();
+            this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnGraphs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblProvince.Location = new System.Drawing.Point(73, 36);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(99, 25);
+            this.lblProvince.TabIndex = 0;
+            this.lblProvince.Text = "Province :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(198, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblAParty
+            // 
+            this.lblAParty.AutoSize = true;
+            this.lblAParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAParty.Location = new System.Drawing.Point(73, 118);
+            this.lblAParty.Name = "lblAParty";
+            this.lblAParty.Size = new System.Drawing.Size(85, 25);
+            this.lblAParty.TabIndex = 2;
+            this.lblAParty.Text = "A party :";
+            // 
+            // lblBParty
+            // 
+            this.lblBParty.AutoSize = true;
+            this.lblBParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBParty.Location = new System.Drawing.Point(70, 188);
+            this.lblBParty.Name = "lblBParty";
+            this.lblBParty.Size = new System.Drawing.Size(86, 25);
+            this.lblBParty.TabIndex = 3;
+            this.lblBParty.Text = "B Party :";
+            // 
+            // lblCParty
+            // 
+            this.lblCParty.AutoSize = true;
+            this.lblCParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCParty.Location = new System.Drawing.Point(70, 259);
+            this.lblCParty.Name = "lblCParty";
+            this.lblCParty.Size = new System.Drawing.Size(88, 25);
+            this.lblCParty.TabIndex = 4;
+            this.lblCParty.Text = "C Party :";
+            // 
+            // lblDParty
+            // 
+            this.lblDParty.AutoSize = true;
+            this.lblDParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDParty.Location = new System.Drawing.Point(73, 323);
+            this.lblDParty.Name = "lblDParty";
+            this.lblDParty.Size = new System.Drawing.Size(87, 25);
+            this.lblDParty.TabIndex = 5;
+            this.lblDParty.Text = "D Party :";
+            this.lblDParty.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblEParty
+            // 
+            this.lblEParty.AutoSize = true;
+            this.lblEParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEParty.Location = new System.Drawing.Point(73, 389);
+            this.lblEParty.Name = "lblEParty";
+            this.lblEParty.Size = new System.Drawing.Size(86, 25);
+            this.lblEParty.TabIndex = 6;
+            this.lblEParty.Text = "E Party :";
+            // 
+            // txBoxAParty
+            // 
+            this.txBoxAParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txBoxAParty.Location = new System.Drawing.Point(198, 118);
+            this.txBoxAParty.Name = "txBoxAParty";
+            this.txBoxAParty.Size = new System.Drawing.Size(267, 30);
+            this.txBoxAParty.TabIndex = 7;
+            // 
+            // txBoxParty
+            // 
+            this.txBoxParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txBoxParty.Location = new System.Drawing.Point(198, 188);
+            this.txBoxParty.Name = "txBoxParty";
+            this.txBoxParty.Size = new System.Drawing.Size(267, 30);
+            this.txBoxParty.TabIndex = 8;
+            // 
+            // txBoxEParty
+            // 
+            this.txBoxEParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txBoxEParty.Location = new System.Drawing.Point(198, 389);
+            this.txBoxEParty.Name = "txBoxEParty";
+            this.txBoxEParty.Size = new System.Drawing.Size(267, 30);
+            this.txBoxEParty.TabIndex = 11;
+            // 
+            // txBoxDParty
+            // 
+            this.txBoxDParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txBoxDParty.Location = new System.Drawing.Point(198, 323);
+            this.txBoxDParty.Name = "txBoxDParty";
+            this.txBoxDParty.Size = new System.Drawing.Size(267, 30);
+            this.txBoxDParty.TabIndex = 10;
+            // 
+            // txCParty
+            // 
+            this.txCParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txCParty.Location = new System.Drawing.Point(198, 256);
+            this.txCParty.Name = "txCParty";
+            this.txCParty.Size = new System.Drawing.Size(267, 30);
+            this.txCParty.TabIndex = 9;
+            // 
+            // btnVote
+            // 
+            this.btnVote.Location = new System.Drawing.Point(55, 468);
+            this.btnVote.Name = "btnVote";
+            this.btnVote.Size = new System.Drawing.Size(448, 45);
+            this.btnVote.TabIndex = 12;
+            this.btnVote.Text = "Vote";
+            this.btnVote.UseVisualStyleBackColor = true;
+            // 
+            // btnStatistics
+            // 
+            this.btnStatistics.Location = new System.Drawing.Point(55, 530);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Size = new System.Drawing.Size(195, 45);
+            this.btnStatistics.TabIndex = 13;
+            this.btnStatistics.Text = "Statistics";
+            this.btnStatistics.UseVisualStyleBackColor = true;
+            // 
+            // btnGraphs
+            // 
+            this.btnGraphs.Location = new System.Drawing.Point(283, 530);
+            this.btnGraphs.Name = "btnGraphs";
+            this.btnGraphs.Size = new System.Drawing.Size(220, 45);
+            this.btnGraphs.TabIndex = 14;
+            this.btnGraphs.Text = "Graphs";
+            this.btnGraphs.UseVisualStyleBackColor = true;
+            // 
+            // FormElection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(163)))), ((int)(((byte)(188)))));
+            this.ClientSize = new System.Drawing.Size(618, 625);
+            this.Controls.Add(this.btnGraphs);
+            this.Controls.Add(this.btnStatistics);
+            this.Controls.Add(this.btnVote);
+            this.Controls.Add(this.txBoxEParty);
+            this.Controls.Add(this.txBoxDParty);
+            this.Controls.Add(this.txCParty);
+            this.Controls.Add(this.txBoxParty);
+            this.Controls.Add(this.txBoxAParty);
+            this.Controls.Add(this.lblEParty);
+            this.Controls.Add(this.lblDParty);
+            this.Controls.Add(this.lblCParty);
+            this.Controls.Add(this.lblBParty);
+            this.Controls.Add(this.lblAParty);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblProvince);
+            this.Name = "FormElection";
+            this.Text = "Election Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblAParty;
+        private System.Windows.Forms.Label lblBParty;
+        private System.Windows.Forms.Label lblCParty;
+        private System.Windows.Forms.Label lblDParty;
+        private System.Windows.Forms.Label lblEParty;
+        private System.Windows.Forms.TextBox txBoxAParty;
+        private System.Windows.Forms.TextBox txBoxParty;
+        private System.Windows.Forms.TextBox txBoxEParty;
+        private System.Windows.Forms.TextBox txBoxDParty;
+        private System.Windows.Forms.TextBox txCParty;
+        private System.Windows.Forms.Button btnVote;
+        private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Button btnGraphs;
+    }
+}
+
---
+++ PartyElection/FormGraphs.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace PartyElection
+{
+    partial class FormGraphs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.grpBoxGraphs = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grpBoxGraphs.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBoxGraphs
+            // 
+            this.grpBoxGraphs.Controls.Add(this.chart1);
+            this.grpBoxGraphs.Location = new System.Drawing.Point(12, 25);
+            this.grpBoxGraphs.Name = "grpBoxGraphs";
+            this.grpBoxGraphs.Size = new System.Drawing.Size(1064, 375);
+            this.grpBoxGraphs.TabIndex = 0;
+            this.grpBoxGraphs.TabStop = false;
+            this.grpBoxGraphs.Text = "graphs";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(51, 38);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(987, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 406);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1064, 274);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // FormGraphs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 692);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grpBoxGraphs);
+            this.Name = "FormGraphs";
+            this.Text = "Graphs";
+            this.grpBoxGraphs.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxGraphs;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
